4eaf294fec341724710244c181961a4fe745c665
ciaiei <tomas.kindstedt@gmail.com>
2023-04-15 18:20:40 +0300

    context, users and creating a connection string

---
+++ backend/Data/eurovisionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class eurovisionContext : DbContext
+    {
+        public eurovisionContext(DbContextOptions<eurovisionContext> options):base(options)
+        {
+            
+        }
+        public DbSet<users> users 
+        { get; set; }
+    }
+}
