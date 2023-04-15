d882551dc522cd2725a740fa596e84bd6bd8fcbe
morski <morski@mor.ski>
2023-04-16 00:00:00 +0300

    Added swagger

--- Eurovision/Program.cs
+++ Eurovision/Program.cs
@@ -1,8 +1,27 @@
+using backend.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+var conStrBuilder = new SqlConnectionStringBuilder()
+{
+    Password = builder.Configuration["DbPassword"],
+    UserID = builder.Configuration["DbUsername"],
+    DataSource = builder.Configuration["DbServer"],
+    InitialCatalog = "eurovision",
+    TrustServerCertificate = true
+};
+
+builder.Services.AddDbContext<EurovisionContext>(options =>
+        options.UseSqlServer(conStrBuilder.ConnectionString));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,6 +30,12 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
