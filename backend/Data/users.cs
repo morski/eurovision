a62c5a2ba796e5a8ffd40248e492a1ad689ab179
ciaiei <tomas.kindstedt@gmail.com>
2023-04-15 18:22:15 +0300

    forgot to save

--- backend/Data/users.cs
+++ backend/Data/users.cs
@@ -1,6 +1,14 @@
-﻿namespace backend.Data
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Data
 {
     public class users
     {
+        public Guid record_guid { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
     }
 }
