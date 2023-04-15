1ad9eddd773002656c1376399e226b256327dfcf
morski <morski@mor.ski>
2023-04-15 13:30:03 +0300

    changed vote to slider

--- frontend/src/components/participant/Vote/vote.tsx
+++ frontend/src/components/participant/Vote/vote.tsx
@@ -3,16 +3,31 @@ import IParticipant from "../../../types/participant.type";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import { StarBorder } from "@mui/icons-material";
+import Slider from "@mui/material/Slider";
 
 type Props = {
     participant: IParticipant,
 };
 
 const Vote: FunctionComponent<Props> = ({ participant }) => {
+
+    function valuetext(value: number) {
+        return `${value}°C`;        
+    }
+
     return (
         <Box>
             <div>Vote Stuff - {participant.name}</div>
-            <Rating name="customized-10" defaultValue={0} max={10} size="large" emptyIcon={	<StarBorder fontSize="inherit" sx={{ color: "white" }} />}/>
+            <Slider
+                aria-label="Vote 1"
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+            />
         </Box>
     );
 };
