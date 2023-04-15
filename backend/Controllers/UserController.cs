d4dc8be83c3a4bef00b218ab5f54e797e0392d69
ciaiei <tomas.kindstedt@gmail.com>
2023-04-15 22:01:45 +0300

    user controls

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Azure.Core;
 
 namespace backend.Controllers
 {
@@ -22,25 +24,88 @@ namespace backend.Controllers
         }
 
         [HttpGet("GetUsers")]
-        public User Get()
+        public IActionResult Get()
         {
-            User user = _context.Users.First(u => u.FirstName == "Tomas");
-            return user;
+            //User user = _context.Users.First(u => u.FirstName == "Tomas");
+            //return user;
+            try { 
+            var users = _context.Users.ToList();
+            if (users.Count == 0 )
+            {
+                return StatusCode(404, "No users found");
+            }
+            return Ok(users);
+            }
+            catch (Exception) 
+            {
+                return StatusCode(500, "An error has occured");
+            }
         }
         [HttpPost("CreateUser")]
-        public User Create([FromBody] UserRequest request)
+        public IActionResult Create([FromBody] UserRequest request)
         {
-            return Ok();
+            //User user = _context.Users.(u => u.FirstName == "Tomas");
+            User user = new User();
+            user.RecordGuid = Guid.NewGuid();
+            user.Username = request.username;
+            user.Password = request.password;
+            user.FirstName = request.first_name;
+            user.LastName = request.last_name;
+            
+            try {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error has occured");
+            }
+            var users = _context.Users.ToList();
+            return Ok(users);
         }
+
         [HttpPut("UpdateUser")]
         public IActionResult Update([FromBody] UserRequest request)
         {
-            return Ok();
+            try {
+                var user = _context.Users.FirstOrDefault(x => x.RecordGuid == request.record_guid);
+                if(user == null)
+                {
+                    return StatusCode(404, "User not found");
+                }
+
+                user.Username = request.username;
+                user.Password = request.password;
+                user.FirstName = request.first_name;
+                user.LastName = request.last_name;
+
+                _context.Entry(user).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (Exception) {
+                return StatusCode(500, "An error has occured");
+            }
+            var users = _context.Users.ToList();
+            return Ok(users);
         }
         [HttpDelete("DeleteUser/{record_guid}")]
-        public IActionResult Delete(Guid record_guid)
+        public IActionResult Delete([FromRoute] Guid record_guid)
         {
-            return Ok();
+            try {
+                var user = _context.Users.FirstOrDefault(x => x.RecordGuid == record_guid);
+                if (user == null)
+                {
+                    return StatusCode(404, "User not found");
+                }
+
+                _context.Entry(user).State = EntityState.Deleted; 
+                _context.SaveChanges();
+            } 
+            catch (Exception) {
+                return StatusCode(500, "An error has occured");
+            }
+            var users = _context.Users.ToList();
+            return Ok(users);
         }
 
         //Test
